cc01e64464dc7c1f06a09dd3aa2ad3e82b446f1a
Balint Orosz <orosz.balint.97@gmail.com>
2022-10-15 20:57:14 +0200

    add cmp + mappings

--- nvim/.config/nvim/lua/user/mappings.lua
+++ nvim/.config/nvim/lua/user/mappings.lua
@@ -4,6 +4,62 @@ local vnoremap = Remap.vnoremap
 local xnoremap = Remap.xnoremap
 local inoremap = Remap.inoremap
 
+-- Telescope
+local builtin = require('telescope.builtin')
+
 nnoremap("<leader>ff", function()
-	require('telescope.builtin').find_files()
+	builtin.find_files()
+end)
+
+nnoremap("<leader>fg", function()
+	builtin.live_grep()
+end)
+
+nnoremap("<leader>fb", function()
+	builtin.buffers()
+end)
+
+
+nnoremap("<leader>fh", function()
+	builtin.help_tags()
+end)
+
+nnoremap("<A-j>", function()
+	vim.cmd[[m .+1<CR>==]]
+end)
+
+nnoremap("<A-k>", function()
+	vim.cmd[[m .-2<CR>==]]
+end)
+
+vnoremap("<A-j>", ":m '>+1<CR>==gv")
+vnoremap("<A-k>", ":m '<-2<CR>==gv")
+
+inoremap("<A-j>", function()
+	vim.cmd[[m .+1<CR>==]]
+end)
+
+inoremap("<A-k>", function()
+	vim.cmd[[m .-2<CR>==]]
+end)
+
+-- Horizontal shift
+
+nnoremap(">", ">>")
+nnoremap("<", "<<")
+
+vnoremap(">", ">gv")
+vnoremap("<", "<gv")
+
+-- Netrw
+
+nnoremap("<leader>r", function()
+	if pcall(vim.cmd, "Rex") then
+	else
+		vim.cmd[[Ex]]
+	end
+end)
+
+nnoremap("<leader>e", function()
+	vim.cmd[[Ex]]
 end)
--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -1,6 +1,11 @@
 local opt = vim.opt
 local g = vim.g
 
+vim.cmd[[colorscheme tokyonight]]
+
+
+local indent = 2
+
 g.mapleader = " "
 opt.clipboard = "unnamedplus"
 opt.cursorline = true
@@ -9,7 +14,10 @@ opt.undofile = true
 -- Numbers
 opt.number = true
 opt.relativenumber = true
-opt.numberwidth = 2
+opt.numberwidth = indent
 opt.ruler = true
+opt.tabstop = indent
+opt.shiftwidth = indent
+opt.scrolloff = 8
 
 opt.completeopt = { "menu", "menuone", "noselect" }
